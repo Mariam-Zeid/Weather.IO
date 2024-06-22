71194aa653983af3a0f9294470d835d863258a5c
MariamZead <mariam.a.zead@gmail.com>
2024-06-22 22:45:51 +0300

    Add current weather fetching functionality

--- src/components/Current Weather/CurrentWeather.jsx
+++ src/components/Current Weather/CurrentWeather.jsx
@@ -1,32 +1,43 @@
 import "./CurrentWeather.css";
-import weatherLogo from "../../assets/img/weather_icons/04d.png";
-export default function CurrentWeather() {
+import { getImageURL } from "../../util/imageUtil";
+import { formatNowDate } from "../../util/DateAndTimeUtil";
+export default function CurrentWeather({ data }) {
   return (
     <section
       className="section current-weather"
       aria-label="current weather"
       data-current-weather
     >
-      <div className="card card-lg current-weather-card">
-        <h2 className="title-2">Now</h2>
-        <div className="wrapper">
-          <p className="heading">
-            25&deg;<sup>c</sup>
-          </p>
-          <img src={weatherLogo} alt="" className="weather-icon" />
+      {data.main ? (
+        <div className="card card-lg current-weather-card">
+          <h2 className="title-2">Now</h2>
+          <div className="wrapper">
+            <p className="heading">
+              {Math.round(data.main.temp)}&deg;<sup>c</sup>
+            </p>
+            <img
+              src={getImageURL(data.weather[0].icon)}
+              alt={data.weather[0].description}
+              className="weather-icon"
+            />
+          </div>
+          <p className="body-3">{data.weather[0].description}</p>
+          <ul className="meta-list">
+            <li className="meta-item">
+              <span className="m-icon">calendar_today</span>
+              <p className="title-3 meta-text">{formatNowDate()}</p>
+            </li>
+            <li className="meta-item">
+              <span className="m-icon">location_on</span>
+              <p className="title-3 meta-text">
+                {data.name.split(" ")[0]}, {data.sys.country}
+              </p>
+            </li>
+          </ul>
         </div>
-        <p className="body-3">Sunny</p>
-        <ul className="meta-list">
-          <li className="meta-item">
-            <span className="m-icon">calendar_today</span>
-            <p className="title-3 meta-text">25 June</p>
-          </li>
-          <li className="meta-item">
-            <span className="m-icon">location_on</span>
-            <p className="title-3 meta-text">Cairo, Egypt</p>
-          </li>
-        </ul>
-      </div>
+      ) : (
+        ""
+      )}
     </section>
   );
 }
--- src/components/Weather Layout/WeatherLayout.jsx
+++ src/components/Weather Layout/WeatherLayout.jsx
@@ -5,26 +5,61 @@ import DaysForecastWeather from "../Days Forecast Weather/DaysForecastWeather";
 import TodaysHighlight from "../Today's Highlight/Today'sHighlight";
 import HourlyForecastWeather from "../Hourly Forecast Weather/HourlyForecastWeather";
 import Footer from "../Footer/Footer";
+import { PositionContext } from "../../context/PositionContext";
+import { useContext, useEffect } from "react";
+import { API_URLS } from "../../util/apiConstants";
+import useFetchWeather from "../../hooks/useFetchWeather";
+import Loading from "../Loading Screen/Loading";
+
 export default function WeatherLayout() {
+  const { latitude, longitude } = useContext(PositionContext);
+
+  // Current Weather API
+  const {
+    isFetching: isFetchingCurrentWeather,
+    fetchWeatherData: fetchCurrentWeather,
+    weatherData: currentWeatherData,
+    error: weatherError,
+  } = useFetchWeather({});
+
+  useEffect(() => {
+    const fetchAllData = async () => {
+      if (latitude !== null && longitude !== null) {
+        await fetchCurrentWeather(API_URLS.currentWeather(latitude, longitude));
+      }
+    };
+    fetchAllData();
+  }, [latitude, longitude]);
+
   return (
     <>
-      <Header />
-      <main
-        id="main"
-        className="w-full max-w-[160rem] mx-auto md:px-12 md:py-12 p-7"
-      >
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:gap-x-16 md:gap-x-6 gap-y-10">
-          <div className="md:col-span-1 flex flex-col gap-10">
-            <CurrentWeather />
-            <DaysForecastWeather />
-          </div>
-          <div className="md:col-span-2 flex flex-col gap-10">
-            <TodaysHighlight />
-            <HourlyForecastWeather />
-          </div>
-        </div>
-      </main>
-      <Footer />
+      {isFetchingCurrentWeather ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <main
+            id="main"
+            className="w-full max-w-[160rem] mx-auto md:px-12 md:py-12 p-7"
+          >
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:gap-x-16 md:gap-x-6 gap-y-10">
+              <div className="md:col-span-1 flex flex-col gap-10">
+                <CurrentWeather
+                  data={currentWeatherData}
+                  loading={isFetchingCurrentWeather}
+                  error={weatherError}
+                />
+                <DaysForecastWeather />
+              </div>
+              <div className="md:col-span-2 flex flex-col gap-10">
+                <TodaysHighlight />
+                <HourlyForecastWeather />
+              </div>
+            </div>
+          </main>
+          <Footer />
+        </>
+      )}
     </>
   );
 }
